b286714860690c376b25a1c76953d93eeefc4f7c
Samantha Flores <samflo121006@gmail.com>
2025-11-14 14:23:56 -0500

    Added unit tests for calculator

--- test_calculator.py
+++ test_calculator.py
@@ -3,12 +3,17 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self): # 3 assertions
+        self.assertEqual(add(2, 2), 4)
+        self.assertEqual(add(4, -2), 2)
+        self.assertEqual(add(-2, -2), -4)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self): # 3 assertions
+        self.assertEqual(subtract(6, 2), 4)
+        self.assertEqual(subtract(-6, 2), -8)
+        self.assertEqual(subtract(2, 6), -4)
+
+    ###########################
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -19,19 +24,20 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self): # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self): # 3 assertions
+        self.assertEqual(log(100, 10), 2)
+        self.assertEqual(log(8, 2), 3)
+        self.assertEqual(log(27, 3), 3)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+    def test_log_invalid_base(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            log(-2, 10)
+
+    ###########################
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
