1d99fab94cb9f84313227fdf709555052eccfa89
SarahZeitouni <sarahmzeitouni@icloud.com>
2025-11-14 14:08:14 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -14,12 +14,18 @@ def div(a,b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero!")
     return b/a
-def log(a,b):
+def logarithm(a,b):
     if a <= 0 or b <= 0:
         raise ValueError("Base and argument must be greater than 0")
     return math.log(b, a)
 def exp(a,b):
     return a**b
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot take square root of negative number")
+    return math.sqrt(a)
+def hypotenuse(a,b):
+    return math.sqrt(a**2 + b**2)
 
 
 
