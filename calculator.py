3e4062f4daa6c5ab2b65ca75564e181654c41b5a
SarahZeitouni <sarahmzeitouni@icloud.com>
2025-11-14 13:25:53 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -12,15 +12,20 @@ def mul(a,b):
     return a*b
 def div(a,b):
     if a == 0:
-        raise ZeroDivisionError("Cannot divide by zero!")
+        raise ZeroDivisionError
     return b/a
 def log(a,b):
     if a <= 0 or b <= 0:
-        raise ValueError("Base and argument must be greater than 0")
+        raise ValueError
     return math.log(b, a)
 def exp(a,b):
     return a**b
-
-
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    else:
+        return math.sqrt(a)
+def hypotenuse(a, b):
+    return math.hypot(a, b)
 
 
